17b4675b369a1343fee2d48391c8e632edfce072
gaoussoukebe <33683408+gaoussoukebe@users.noreply.github.com>
2017-12-21 01:00:23 +0300

    Sensors are back 2! The Services

---
+++ app/src/main/java/btu/treasurehunt/Cellsservice.java
@@ -0,0 +1,25 @@
+package btu.treasurehunt;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface Cellsservice {
+    @GET("accounts") /** Gets data from server app https://restfulap.herokuapp.com/accounts **/
+    Call<List<Account>> all();
+
+    @GET("accounts/{isbn}")
+    Call<Account> get(@Path("isbn") String id);
+
+    @POST("accounts/new")
+    Call<Account> create(@Body Account account);
+
+
+
+
+
+}
---
+++ app/src/main/java/btu/treasurehunt/Sensors/sensorBatchservice.java
@@ -0,0 +1,26 @@
+package btu.treasurehunt.Sensors;
+
+import java.util.List;
+
+import btu.treasurehunt.Account;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface sensorBatchservice {
+    @GET("accounts") /** Gets data from server app https://restfulap.herokuapp.com/accounts **/
+    Call<List<Account>> all();
+
+    @GET("accounts/{isbn}")
+    Call<Account> get(@Path("isbn") String id);
+
+    @POST("accounts/new")
+    Call<Account> create(@Body Account account);
+
+
+
+
+
+}
