fbf637ce28a2fe6b1bde075f45dfa2b481f1798c
gaoussoukebe <33683408+gaoussoukebe@users.noreply.github.com>
2018-01-07 15:34:24 +0300

    OK

--- app/src/main/java/btu/treasurehunt/SplashActivity.java
+++ app/src/main/java/btu/treasurehunt/SplashActivity.java
@@ -10,6 +10,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -18,77 +19,139 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.util.ArrayList;
+
+import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
 
 public class SplashActivity extends AppCompatActivity {
     static final int REQUEST_LOCATION = 1;
     LocationManager locationManager;
+    private LocationRequest mLocationRequest;
+    private long UPDATE_INTERVAL = 10 * 1000;  /* 10 secs */
+    private long FASTEST_INTERVAL = 2000; /* 2 sec */
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
-        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        getLocation();
+        startLocationUpdates();
         // Start home activity
 
 
     }
 
-    void getLocation() {
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
+    protected void startLocationUpdates() {
 
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        // Create the location request to start receiving updates
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
 
-        } else {
-            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        // Create LocationSettingsRequest object using location request
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        LocationSettingsRequest locationSettingsRequest = builder.build();
 
-            if (location != null){
-                double latti = location.getLatitude();
-                double longi = location.getLongitude();
+        // Check whether location settings are satisfied
+        // https://developers.google.com/android/reference/com/google/android/gms/location/SettingsClient
+        SettingsClient settingsClient = LocationServices.getSettingsClient(this);
+        settingsClient.checkLocationSettings(locationSettingsRequest);
 
-                ArrayList<LatLng> campusOptions = new ArrayList<LatLng>();
-                campusOptions.add(new LatLng(40.189982, 29.127641));
-                campusOptions.add(new LatLng(40.186695, 29.128242));
-                campusOptions.add(new LatLng(40.186199, 29.130753));
-                campusOptions.add(new LatLng(40.189920, 29.130887));
-                campusOptions.add(new LatLng(40.189982, 29.127641));
-                if(isPointInPolygon(new LatLng(latti,longi),campusOptions)) {
-                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                    // close splash activity
-                    finish();
-                }
-                else{
-                    Toast.makeText(this , "Lattitude " + latti + "Longitude " + longi , Toast.LENGTH_LONG).show();
-                }
-                //  ((EditText)findViewById(R.id.etLocationLong)).setText("Longitude: " + longi);
-            } else {
-                Toast.makeText(this , "We can't locate your current position!" , Toast.LENGTH_LONG).show();
-            }
+        // new Google API SDK v11 uses getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
+        getFusedLocationProviderClient(this).requestLocationUpdates(mLocationRequest, new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        Location location=locationResult.getLastLocation();
+                        if (location != null){
+                            double latti = location.getLatitude();
+                            double longi = location.getLongitude();
 
+                            ArrayList<LatLng> campusOptions = new ArrayList<LatLng>();
+                            campusOptions.add(new LatLng(40.189982, 29.127641));
+                            campusOptions.add(new LatLng(40.186695, 29.128242));
+                            campusOptions.add(new LatLng(40.186199, 29.130753));
+                            campusOptions.add(new LatLng(40.189920, 29.130887));
+                            campusOptions.add(new LatLng(40.189982, 29.127641));
+                            if(isPointInPolygon(new LatLng(latti,longi),campusOptions)) {
+                                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                                // close splash activity
+                                finish();
+                            }
+                            else{
+                                Toast.makeText(getBaseContext() , "Lattitude " + latti + "Longitude " + longi , Toast.LENGTH_LONG).show();
+                            }
+                            //  ((EditText)findViewById(R.id.etLocationLong)).setText("Longitude: " + longi);
+                        } else {
+                            Toast.makeText(getBaseContext() , "We can't locate your current position!" , Toast.LENGTH_LONG).show();
+                        }
+                        onLocationChanged(locationResult.getLastLocation());
+                    }
+                },
+                Looper.myLooper());
     }
 
+    public void getLastLocation() {
+        // Get last known recent location using new Google Play Services SDK (v11+)
+        FusedLocationProviderClient locationClient = getFusedLocationProviderClient(this);
 
-
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        switch (requestCode) {
-            case REQUEST_LOCATION:
-                getLocation();
-                break;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
+        locationClient.getLastLocation()
+                .addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // GPS location can be null if GPS is switched off
+                        if (location != null) {
+                            onLocationChanged(location);
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("MapDemoActivity", "Error trying to get last GPS location");
+                        e.printStackTrace();
+                    }
+                });
+    }
+    public void onLocationChanged(Location location) {
+        // New location has now been determined
+        String msg = "Updated Location: " +
+                Double.toString(location.getLatitude()) + "," +
+                Double.toString(location.getLongitude());
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        // You can now create a LatLng Object for use with maps
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
     }
 
 
